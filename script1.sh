16dcf4ca234b7f7a74423af27302014da7015cc3
TDI <TDI@gmail.com>
2026-02-04 15:49:21 +0530

    this is commit in main

--- script1.sh
+++ script1.sh
@@ -2,3 +2,6 @@ echo "We are trying this practical from linux EC2"
 echo "We are from TDI and learning git/gihub"
 echo "I am from Pune-Maharshtra"
 echo "We are form TDI-devops_Sep-Batch"
+echo "hello good evening"
+echo "i am working"
+
