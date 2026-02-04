c0f16f0d95aeeb1ea1a6de8d7e30233af3f23c8b
TDI <TDI@gmail.com>
2026-02-04 15:58:25 +0530

    this is commit in b2

--- script1.sh
+++ script1.sh
@@ -2,5 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "We are from TDI and learning git/gihub"
 echo "I am from Pune-Maharshtra"
 echo "We are form TDI-devops_Sep-Batch"
-echo "we are adding in b1"
+echo "this is chane in b2"
 
